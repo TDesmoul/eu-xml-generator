bfcf5e93251da0dcb3fbbe88e4d4b367fb09fbd8
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2019-03-11 17:06:08 +0100

    récupération d'un noeud ingredient à partir du cas_number

--- app/services/xml_ingredient/retrieve.rb
+++ app/services/xml_ingredient/retrieve.rb
@@ -1,8 +1,13 @@
 class XmlIngredient::Retrieve
   def self.call(file_name, cas_number)
-    file_path = Rails.root + "lib/banque_ingredient_1.xml" # "lib/#{file_name}"
+    # download file from FTP :
+    ftp_repo = "/source_xmls/ingredients"
+    local_repo = "#{Rails.root}/lib/xml_files/"
+    PfFtp.new.download_file(ftp_repo, local_repo, file_name)
+    # retrieve xml ingredient with cas_number :
+    file_path = Rails.root + "lib/xml_files/#{file_name}"
     xml_file = File.open(file_path)
     xml_doc = Nokogiri::XML(xml_file)
-    xml_doc.xpath("//CasNumber[text()='#{cas_number}']").first
+    xml_doc.xpath("//Ingredient//CasNumber[text()='#{cas_number}']").first.parent
   end
 end
