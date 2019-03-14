b88b3e3512156970c27a6a04fd253fda5a1b3b28
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2019-03-14 12:49:20 +0100

    bg job + home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/gogogo', to: 'pages#create_xmls', as: 'gogogo'
+  get '/job_in_progress', to: 'pages#job_in_progress', as: 'job_in_progress'
   # Sidekiq Web UI, only for admins.
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
