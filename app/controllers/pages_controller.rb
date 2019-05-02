ce1986e297aaa66351f416156f3ee097d115ce52
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2019-05-02 16:33:55 +0200

    mise en place du background job, et de l'interface utilisateur pour l'ajout des AnnualSalesDatas dans les XML

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     missing_emission_xml = MissingXml.find_by(xml_type: "emission")
     @missing_emission_cas = missing_emission_xml.elements if missing_emission_xml
     @job_in_progress = Sidekiq::Workers.new.size > 0
+    @feedback = AnnualSalesUpdateFeedback.last
   end
   def check_ingredients_and_emissions
     CheckIngredientsAndEmissionsJob.perform_later
@@ -17,6 +18,24 @@ class PagesController < ApplicationController
   end
   def create_xmls
     CheckDatasAndCreateXmlsJob.perform_later #.set(queue: :critical).perform_later
+    respond_to do |format|
+      format.html do
+        redirect_to root_path
+      end
+      format.js
+    end
+  end
+  def add_annual_sales
+    AddAnnualSalesDatasJob.perform_later #.set(queue: :critical).perform_later
+    respond_to do |format|
+      format.html do
+        redirect_to root_path
+      end
+      format.js
+    end
+  end
+  def destroy_feedback
+    AnnualSalesUpdateFeedback.destroy_all
     respond_to do |format|
       format.html do
         redirect_to root_path
